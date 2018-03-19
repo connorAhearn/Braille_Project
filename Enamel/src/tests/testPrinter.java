f010bdd40863ec09c7382667fd62bf88d5232ebe
connorAhearn <connor.ahearn305@gmail.com>
2018-03-19 19:52:40 -0400

    Fixed Printer Bug

--- Enamel/src/tests/testPrinter.java
+++ Enamel/src/tests/testPrinter.java
@@ -75,6 +75,27 @@ public class testPrinter {
 		assertEquals("", reader.nextLine());
 	}
 	
+	public void checkOldBlock(ArrayList<Block> blocks, Block block) throws InvalidCellException {
+		if(first) first = false;
+		else assertEquals("/~NEXTT", reader.nextLine());
+		assertEquals("/~disp-clearAll", reader.nextLine());
+		assertEquals("/~disp-cell-pins:0 " + interpreter.getPins(block.letter), reader.nextLine());
+		assertEquals(block.story, reader.nextLine());
+		assertEquals("/~skip-button:0 ONEE", reader.nextLine());
+		assertEquals("/~skip-button:1 TWOO", reader.nextLine());
+		assertEquals("/~user-input", reader.nextLine());
+		assertEquals("/~ONEE", reader.nextLine());
+		assertEquals((block.answer == 1) ? "/~sound:correct.wav" : "/~sound:wrong.wav", reader.nextLine());
+		assertEquals((block.answer == 1) ? block.correctResponse : block.wrongResponse, reader.nextLine());
+		assertEquals("/~skip:NEXTT", reader.nextLine());
+		assertEquals("/~TWOO", reader.nextLine());
+		assertEquals((block.answer == 2) ? "/~sound:correct.wav" : "/~sound:wrong.wav", reader.nextLine());
+		assertEquals((block.answer == 2) ? block.correctResponse : block.wrongResponse, reader.nextLine());
+		assertEquals("/~skip:" + blocks.get(blocks.indexOf(block) + 1), reader.nextLine());
+		assertEquals("", reader.nextLine());
+	}
+	
+	/*
 	private void checkNewBlock(Block block) throws InvalidCellException {
 		if(first) first = false;
 		else assertEquals("/~" + block.name.toUpperCase(), reader.nextLine());
@@ -89,7 +110,28 @@ public class testPrinter {
 			assertEquals("/~" + "JUMPP" + i, reader.nextLine());
 			assertEquals((block.answer == i) ? "/~sound:correct.wav" : "/~sound:wrong.wav", reader.nextLine());
 			assertEquals((block.answer == i) ? block.correctResponse : block.wrongResponse, reader.nextLine());
-			assertEquals("/~skip:NEXTT", reader.nextLine());
+			assertEquals("/~skip:" + blocks.get(index + 1).name.toUpperCase(), reader.nextLine());
+		}
+		assertEquals("", reader.nextLine());
+	}
+	*/
+	
+	private void checkNewBlock(ArrayList<Block> blocks, Block block) throws InvalidCellException {
+		if(first) first = false;
+		else assertEquals("/~" + block.name.toUpperCase(), reader.nextLine());
+		assertEquals("/~disp-clearAll", reader.nextLine());
+		assertEquals("/~disp-string:" + block.cells, reader.nextLine());
+		assertEquals(block.story, reader.nextLine());
+		for(int i = 1; i <= block.buttonsUsed; i++) {
+			assertEquals("/~skip-button:" + i + " " + "JUMPP" + i , reader.nextLine());
+		}
+		assertEquals("/~user-input", reader.nextLine());
+		for(int i = 1; i <= block.buttonsUsed; i++) {
+			assertEquals("/~" + "JUMPP" + i, reader.nextLine());
+			assertEquals((block.answer == i) ? "/~sound:correct.wav" : "/~sound:wrong.wav", reader.nextLine());
+			assertEquals((block.answer == i) ? block.correctResponse : block.wrongResponse, reader.nextLine());
+			if(blocks.indexOf(block) < blocks.size() - 1) 
+				assertEquals("/~skip:" + blocks.get(blocks.indexOf(block) + 1).name.toUpperCase(), reader.nextLine());
 		}
 		assertEquals("", reader.nextLine());
 	}
@@ -127,12 +169,14 @@ public class testPrinter {
 	public void test1OldBlock() throws IOException, InvalidBlockException, InvalidCellException, OddSpecialCharacterException {
 		printer = new Printer("test.txt");
 		Block tBlock = new Block("name", "hi", "yes", "no", 1, 'c', 2);
-		printer.addBlock(tBlock);
+		ArrayList<Block> blocks = new ArrayList<>();
+		blocks.add(tBlock);
+		printer.addBlockList(blocks);
 		printer.print();
 		reader = new Scanner(file);
 		
 		initialBlock(1, 4);
-		checkNewBlock(tBlock);
+		checkNewBlock(blocks, tBlock);
 	}
 	
 	@Test
@@ -140,14 +184,16 @@ public class testPrinter {
 		printer = new Printer("test.txt", 1, 4);
 		Block tBlock1 = new Block("name", "hi", "yes", "no", 1, 'c');
 		Block tBlock2 = new Block("name", "hello", "yep", "nope", 2, 'd');
-		printer.addBlock(tBlock1);
-		printer.addBlock(tBlock2);
+		ArrayList<Block> blocks = new ArrayList<>();
+		blocks.add(tBlock1);
+		blocks.add(tBlock2);
+		printer.addBlockList(blocks);
 		printer.print();
 		reader = new Scanner(file);
 		
 		initialBlock(1, 4);
-		checkNewBlock(tBlock1);
-		checkNewBlock(tBlock2);
+		checkNewBlock(blocks, tBlock1);
+		checkNewBlock(blocks, tBlock2);
 	}
 	
 	@Test
@@ -163,8 +209,8 @@ public class testPrinter {
 		reader = new Scanner(file);
 		
 		initialBlock(1, 4);
-		checkNewBlock(tBlock1);
-		checkNewBlock(tBlock2);
+		checkNewBlock(blockList, tBlock1);
+		checkNewBlock(blockList, tBlock2);
 	}
 	
 	@Test
@@ -178,7 +224,7 @@ public class testPrinter {
 		reader = new Scanner(file);
 		
 		initialBlock(1, 4);
-		checkNewBlock(tBlock1);
+		checkNewBlock(blockList, tBlock1);
 	}
 	
 	@Test
@@ -194,31 +240,37 @@ public class testPrinter {
 	public void testNoCorrect() throws IOException, InvalidBlockException, InvalidCellException, OddSpecialCharacterException {
 		printer = new Printer("test.txt", 5, 6);
 		Block tBlock = new Block("name", "hey!", "", "no", 3, 'c', 5);
-		printer.addBlock(tBlock);
+		ArrayList<Block> blocks = new ArrayList<>();
+		blocks.add(tBlock);
+		printer.addBlockList(blocks);
 		printer.print();
 		reader = new Scanner(file);
 		
 		initialBlock(5, 6);
-		checkNewBlock(tBlock);
+		checkNewBlock(blocks, tBlock);
 	}
 	
 	@Test
 	public void testNoIncorrect() throws IOException, InvalidBlockException, InvalidCellException, OddSpecialCharacterException {
 		printer = new Printer("test.txt", 5, 6);
 		Block tBlock = new Block("name", "hey!", "yes", "", 3, 'c', 5);
-		printer.addBlock(tBlock);
+		ArrayList<Block> blocks = new ArrayList<>();
+		blocks.add(tBlock);
+		printer.addBlockList(blocks);
 		printer.print();
 		reader = new Scanner(file);
 		
 		initialBlock(5, 6);
-		checkNewBlock(tBlock);
+		checkNewBlock(blocks, tBlock);
 	}
 	
 	@Test
 	public void testSoundTag() throws IOException, InvalidBlockException, InvalidCellException, OddSpecialCharacterException {
 		printer = new Printer("test.txt", 5, 6);
 		Block tBlock = new Block("name", "hey! <correct.wav>", "yes", "", 3, 'c', 5);
-		printer.addBlock(tBlock);
+		ArrayList<Block> blocks = new ArrayList<>();
+		blocks.add(tBlock);
+		printer.addBlockList(blocks);
 		printer.print();
 		reader = new Scanner(file);
 		
@@ -238,7 +290,8 @@ public class testPrinter {
 			assertEquals("/~" + "JUMPP" + i, reader.nextLine());
 			assertEquals((tBlock.answer == i) ? "/~sound:correct.wav" : "/~sound:wrong.wav", reader.nextLine());
 			assertEquals((tBlock.answer == i) ? tBlock.correctResponse : tBlock.wrongResponse, reader.nextLine());
-			assertEquals("/~skip:NEXTT", reader.nextLine());
+			if(i <= blocks.size() - 1)
+				assertEquals("/~skip:" + blocks.get(i + 1).name.toUpperCase(), reader.nextLine());
 		}
 		assertEquals("", reader.nextLine());
 	}
@@ -247,7 +300,9 @@ public class testPrinter {
 	public void testSoundTag2() throws IOException, InvalidBlockException, InvalidCellException, OddSpecialCharacterException {
 		printer = new Printer("test.txt", 5, 6);
 		Block tBlock = new Block("name", "hey! <correct.wav> nice", "yes", "", 3, 'c', 5);
-		printer.addBlock(tBlock);
+		ArrayList<Block> blocks = new ArrayList<>();
+		blocks.add(tBlock);
+		printer.addBlockList(blocks);
 		printer.print();
 		reader = new Scanner(file);
 		
@@ -268,7 +323,8 @@ public class testPrinter {
 			assertEquals("/~" + "JUMPP" + i, reader.nextLine());
 			assertEquals((tBlock.answer == i) ? "/~sound:correct.wav" : "/~sound:wrong.wav", reader.nextLine());
 			assertEquals((tBlock.answer == i) ? tBlock.correctResponse : tBlock.wrongResponse, reader.nextLine());
-			assertEquals("/~skip:NEXTT", reader.nextLine());
+			if(i <= blocks.size() - 1)
+				assertEquals("/~skip:" + blocks.get(i + 1).name.toUpperCase(), reader.nextLine());
 		}
 		assertEquals("", reader.nextLine());
 	}
@@ -277,7 +333,9 @@ public class testPrinter {
 	public void testSoundTag3() throws IOException, InvalidBlockException, InvalidCellException, OddSpecialCharacterException {
 		printer = new Printer("test.txt", 5, 6);
 		Block tBlock = new Block("name", "<correct.wav> nice", "yes", "", 3, 'c', 5);
-		printer.addBlock(tBlock);
+		ArrayList<Block> blocks = new ArrayList<>();
+		blocks.add(tBlock);
+		printer.addBlockList(blocks);
 		printer.print();
 		reader = new Scanner(file);
 		
@@ -298,7 +356,8 @@ public class testPrinter {
 			assertEquals("/~JUMPP" + i, reader.nextLine());
 			assertEquals((tBlock.answer == i) ? "/~sound:correct.wav" : "/~sound:wrong.wav", reader.nextLine());
 			assertEquals((tBlock.answer == i) ? tBlock.correctResponse : tBlock.wrongResponse, reader.nextLine());
-			assertEquals("/~skip:NEXTT", reader.nextLine());
+			if(i <= blocks.size() - 1)
+				assertEquals("/~skip:" + blocks.get(i + 1).name.toUpperCase(), reader.nextLine());
 		}
 		assertEquals("", reader.nextLine());
 	}
@@ -307,7 +366,9 @@ public class testPrinter {
 	public void testSoundTag4() throws IOException, InvalidBlockException, InvalidCellException, OddSpecialCharacterException {
 		printer = new Printer("test.txt", 5, 6);
 		Block tBlock = new Block("name", "<correct.wav>", "yes", "", 3, 'c', 5);
-		printer.addBlock(tBlock);
+		ArrayList<Block> blocks = new ArrayList<>();
+		blocks.add(tBlock);
+		printer.addBlockList(blocks);
 		printer.print();
 		reader = new Scanner(file);
 		
@@ -327,7 +388,8 @@ public class testPrinter {
 			assertEquals("/~JUMPP" + i, reader.nextLine());
 			assertEquals((tBlock.answer == i) ? "/~sound:correct.wav" : "/~sound:wrong.wav", reader.nextLine());
 			assertEquals((tBlock.answer == i) ? tBlock.correctResponse : tBlock.wrongResponse, reader.nextLine());
-			assertEquals("/~skip:NEXTT", reader.nextLine());
+			if(i <= blocks.size() - 1)
+				assertEquals("/~skip:" + blocks.get(i + 1).name.toUpperCase(), reader.nextLine());
 		}
 		assertEquals("", reader.nextLine());
 	}
@@ -381,7 +443,9 @@ public class testPrinter {
 	public void testPinTag() throws IOException, InvalidBlockException, InvalidCellException, OddSpecialCharacterException {
 		printer = new Printer("test.txt", 6, 6);
 		Block tBlock = new Block("name", "hey! *waffle* nice", "yes", "", 3, 'c', 5);
-		printer.addBlock(tBlock);
+		ArrayList<Block> blocks = new ArrayList<>();
+		blocks.add(tBlock);
+		printer.addBlockList(blocks);
 		printer.print();
 		reader = new Scanner(file);
 		
@@ -402,7 +466,8 @@ public class testPrinter {
 			assertEquals("/~JUMPP" + i, reader.nextLine());
 			assertEquals((tBlock.answer == i) ? "/~sound:correct.wav" : "/~sound:wrong.wav", reader.nextLine());
 			assertEquals((tBlock.answer == i) ? tBlock.correctResponse : tBlock.wrongResponse, reader.nextLine());
-			assertEquals("/~skip:NEXTT", reader.nextLine());
+			if(i <= blocks.size() - 1)
+				assertEquals("/~skip:" + blocks.get(i + 1).name.toUpperCase(), reader.nextLine());
 		}
 		assertEquals("", reader.nextLine());
 	}
@@ -411,7 +476,9 @@ public class testPrinter {
 	public void testPinTag1() throws IOException, InvalidBlockException, InvalidCellException, OddSpecialCharacterException {
 		printer = new Printer("test.txt", 6, 6);
 		Block tBlock = new Block("name", "*waffle* nice", "yes", "", 3, 'c', 5);
-		printer.addBlock(tBlock);
+		ArrayList<Block> blocks = new ArrayList<>();
+		blocks.add(tBlock);
+		printer.addBlockList(blocks);
 		printer.print();
 		reader = new Scanner(file);
 		
@@ -432,7 +499,8 @@ public class testPrinter {
 			assertEquals("/~JUMPP" + i, reader.nextLine());
 			assertEquals((tBlock.answer == i) ? "/~sound:correct.wav" : "/~sound:wrong.wav", reader.nextLine());
 			assertEquals((tBlock.answer == i) ? tBlock.correctResponse : tBlock.wrongResponse, reader.nextLine());
-			assertEquals("/~skip:NEXTT", reader.nextLine());
+			if(i <= blocks.size() - 1)
+				assertEquals("/~skip:" + blocks.get(i + 1).name.toUpperCase(), reader.nextLine());
 		}
 		assertEquals("", reader.nextLine());
 	}
@@ -441,7 +509,9 @@ public class testPrinter {
 	public void testPinTag2() throws IOException, InvalidBlockException, InvalidCellException, OddSpecialCharacterException {
 		printer = new Printer("test.txt", 6, 6);
 		Block tBlock = new Block("name", "nice *waffle*", "yes", "", 3, 'c', 5);
-		printer.addBlock(tBlock);
+		ArrayList<Block> blocks = new ArrayList<>();
+		blocks.add(tBlock);
+		printer.addBlockList(blocks);
 		printer.print();
 		reader = new Scanner(file);
 		
@@ -461,7 +531,8 @@ public class testPrinter {
 			assertEquals("/~JUMPP" + i, reader.nextLine());
 			assertEquals((tBlock.answer == i) ? "/~sound:correct.wav" : "/~sound:wrong.wav", reader.nextLine());
 			assertEquals((tBlock.answer == i) ? tBlock.correctResponse : tBlock.wrongResponse, reader.nextLine());
-			assertEquals("/~skip:NEXTT", reader.nextLine());
+			if(i <= blocks.size() - 1)
+				assertEquals("/~skip:" + blocks.get(i + 1).name.toUpperCase(), reader.nextLine());
 		}
 		assertEquals("", reader.nextLine());
 	}
